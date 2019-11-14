696fb46d7deab81c409a511d5044e4c0e35b757f
wangdq.no1@gmail.com <wangdq.no1@gmail.com>
2019-11-14 16:40:44 +0800

    test add git

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# system_test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/wangdaqiang123/system_test.git
+git push -u origin master
